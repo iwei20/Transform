2e8d751466e62d2f10d825de867fb701f461a65f
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2021-03-16 19:29:01 -0400

    Add a transform manager to hold transformation matrix

---
+++ transform_manager.cpp
@@ -0,0 +1,55 @@
+#include "transform_manager.hpp"
+
+#include <cmath>
+transform_manager::transform_manager() : m{4, 4} {}
+
+const double convert_rad = M_PI / 180;
+void transform_manager::translate(double x_units, double y_units, double z_units) {
+    matrix to_mult = matrix::ident(4);
+    to_mult[3][0] = x_units;
+    to_mult[3][1] = y_units;
+    to_mult[3][2] = z_units;
+    m = to_mult * m;
+}
+
+void transform_manager::dilate(double x_scale, double y_scale, double z_scale) {
+    matrix to_mult = matrix::ident(4);
+    to_mult[0][0] = x_scale;
+    to_mult[1][1] = y_scale;
+    to_mult[2][2] = z_scale;
+    m = to_mult * m;
+}
+
+void transform_manager::xRot(double degrees) {
+    matrix to_mult = matrix::ident(4);
+    double radians = degrees * convert_rad;
+    to_mult[1][1] = cos(radians);
+    to_mult[1][2] = -sin(radians);
+    to_mult[2][1] = sin(radians);
+    to_mult[2][2] = cos(radians);
+    m = to_mult * m;
+}
+
+void transform_manager::yRot(double degrees) {
+    matrix to_mult = matrix::ident(4);
+    double radians = degrees * convert_rad;
+    to_mult[2][2] = cos(radians);
+    to_mult[2][0] = -sin(radians);
+    to_mult[0][2] = sin(radians);
+    to_mult[0][0] = cos(radians);
+    m = to_mult * m;
+}
+
+void transform_manager::zRot(double degrees) {
+    matrix to_mult = matrix::ident(4);
+    double radians = degrees * convert_rad;
+    to_mult[0][0] = cos(radians);
+    to_mult[0][1] = -sin(radians);
+    to_mult[1][0] = sin(radians);
+    to_mult[1][1] = cos(radians);
+    m = to_mult * m;
+}
+
+matrix& transform_manager::get_matrix() {
+    return m;
+}
---
+++ transform_manager.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "matrix.hpp"
+class transform_manager {
+    private:
+        matrix m;
+    public:
+        transform_manager();
+        void translate(double x_units, double y_units, double z_units);
+        void dilate(double x_scale, double y_scale, double z_scale);
+        void xRot(double degrees);
+        void yRot(double degrees);
+        void zRot(double degrees);
+        matrix& get_matrix();
+};

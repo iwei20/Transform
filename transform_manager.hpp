bfdc69a27cd94db999edea485680581cd416287d
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2021-03-17 18:46:45 -0400

    Document transform_manager

--- transform_manager.hpp
+++ transform_manager.hpp
@@ -5,12 +5,43 @@ class transform_manager {
     private:
         matrix m;
     public:
+        /**
+         * Creates an object that holds a transformation matrix, initialized to an identity matrix, which can be operated on.
+         **/
         transform_manager();
+
+        /**
+         * Applies a translation to the matrix.
+         **/
         void translate(double x_units, double y_units, double z_units);
+
+        /**
+         * Applies a dilation to the matrix.
+         **/
         void dilate(double x_scale, double y_scale, double z_scale);
+
+        /**
+         * Applies a rotation around the x-axis to the matrix.
+         **/
         void xRot(double degrees);
+
+        /**
+         * Applies a rotation around the y-axis to the matrix.
+         **/
         void yRot(double degrees);
+
+        /**
+         * Applies a rotation around the z-axis to the matrix.
+         **/
         void zRot(double degrees);
+
+        /**
+         * Returns a reference to the matrix.
+         **/
         matrix& get_matrix();
+
+        /**
+         * Replaces the matrix reference with a new identity matrix.
+         **/
         void reset();
 };
